e27a403ce7fad117d0da7457007666c67a777708
chenqim <chenqim@51bsi.com>
2019-06-27 16:43:16 +0800

    add 202

---
+++ Q200-Q300/202_Happy_number.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+  let arr = n.toString().split('')
+  // 已经出现过的数字数组
+  let temp = []
+  while (true) {
+      let sum = 0
+      for (let i = 0; i < arr.length; i++) {
+          sum += Math.pow(Number(arr[i]), 2)
+      }
+      // 为1就是快乐数
+      if (sum === 1) return true
+      // 如果存在则直接跳出循环（因为已存在的会进入一个循环永远都不会是一个快乐数）
+      if (temp.indexOf(sum) > -1) return false
+      // 如果不存在则讲其放入数组中
+      temp.push(sum)
+      // 重新给arr赋值
+      arr = sum.toString().split('')
+  }
+};
+
+console.log(isHappy(101))
