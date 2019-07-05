fc2ec4460d9130fa6131cea115faaa225f0740f6
chenqim <chenqim@51bsi.com>
2019-07-05 17:41:10 +0800

    add 020

---
+++ Q001-Q100/020_Valid_Parentheses.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  s = s.replace(/\s/g, '')
+  let stack = []
+  for (let i = 0; i < s.length; i ++) {
+      // 如果是左括号则入栈 如果是右括号则与出栈的值相比 如果不同则是无效的
+      if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+          stack.push(s[i])
+      } else if (s[i] === ')') {
+          if (stack.pop() !== '(') {
+              return false
+          }
+      } else if (s[i] === '}') {
+          if (stack.pop() !== '{') {
+              return false
+          }
+      }  else if (s[i] === ']') {
+          if (stack.pop() !== '[') {
+              return false
+          }
+      } 
+  }
+  // 如果 stack 中还有多余的未匹配则说明是无效的
+  return !stack.length
+};
+
+console.log(isValid('()[({}){}[]](){}'))
