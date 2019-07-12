e33c372182b48c7a04a4cab8f9aae2a57b16a59f
chenqim <chenqim@51bsi.com>
2019-07-12 13:57:10 +0800

    add 167

---
+++ Q101-Q200/167_Two_Sum_II_-_Input_array_is_sorted.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) return [i + 1, j + 1]
+        }
+    }
+};
+
+// 方法二：利用已经排序的特点，用双指针
+var twoSum2 = function(numbers, target) {
+    let low = 0
+    let high = numbers.length - 1
+    while (low < high) {
+        if (numbers[low] + numbers[high] === target) return [low + 1, high + 1]
+        if (numbers[low] + numbers[high] > target) high--
+        if (numbers[low] + numbers[high] < target) low++
+    }
+};
+
+console.log(twoSum2([2, 7, 11, 15], 9))
