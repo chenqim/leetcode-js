87d944553a55f0dfb7d5377fe443e9db707b6b48
chenqim <chenqim@51bsi.com>
2019-09-12 10:21:20 +0800

    add 796

---
+++ Q700-Q799/796_Rotate_String.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+var rotateString = function(A, B) {
+  return A.length === B.length && (A + A).indexOf(B) > -1
+};
+
+console.log(rotateString('abcde', 'cdeab'))
