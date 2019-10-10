40ca2305cafaa450dd9b75b34b66c1da39cedcd6
chenqim <chenqim@51bsi.com>
2019-10-10 14:13:18 +0800

    add 896

---
+++ Q800-Q899/896_Monotonic_Array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var isMonotonic = function(A) {
+  let flag = ''
+  if (A[A.length - 1] > A[0]) {
+      flag = 'up'
+  } else {
+      flag = 'down'
+  }
+  if (flag === 'up') {
+      for (let i = 1; i < A.length; i++) {
+          if (A[i] < A[i - 1]) return false
+      }
+      return true
+  } else {
+      for (let i = 1; i < A.length; i++) {
+          if (A[i] > A[i - 1]) return false
+      }
+      return true
+  }
+};
+
+console.log(isMonotonic([2, 4, 5, 8]))
