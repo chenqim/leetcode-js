4ced18f6ff668618bbb38d1385871e128c1b2c64
chenqim <chenqim@51bsi.com>
2019-10-10 14:13:55 +0800

    add 896

--- Q800-Q899/896_Monotonic_Array.js
+++ Q800-Q899/896_Monotonic_Array.js
@@ -3,23 +3,23 @@
  * @return {boolean}
  */
 var isMonotonic = function(A) {
-  let flag = ''
-  if (A[A.length - 1] > A[0]) {
-      flag = 'up'
-  } else {
-      flag = 'down'
-  }
-  if (flag === 'up') {
-      for (let i = 1; i < A.length; i++) {
-          if (A[i] < A[i - 1]) return false
-      }
-      return true
-  } else {
-      for (let i = 1; i < A.length; i++) {
-          if (A[i] > A[i - 1]) return false
-      }
-      return true
-  }
+    let flag = ''
+    if (A[A.length - 1] > A[0]) {
+        flag = 'up'
+    } else {
+        flag = 'down'
+    }
+    if (flag === 'up') {
+        for (let i = 1; i < A.length; i++) {
+            if (A[i] < A[i - 1]) return false
+        }
+        return true
+    } else {
+        for (let i = 1; i < A.length; i++) {
+            if (A[i] > A[i - 1]) return false
+        }
+        return true
+    }
 };
 
 console.log(isMonotonic([2, 4, 5, 8]))
