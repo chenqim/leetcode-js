2d8a26612e139bfad2f1410faa640945e291a30b
chenqim <chenqim@51bsi.com>
2020-04-29 10:40:52 +0800

    add 1266

---
+++ Q1200-Q1299/1266_Minimum_Time_Visiting_All_Points.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    let result = 0
+    for (let i = 1; i < points.length; i++) {
+        let x = Math.abs(points[i][0] - points[i - 1][0])
+        let y = Math.abs(points[i][1] - points[i - 1][1])
+        result += Math.max(x, y)
+    }
+    return result
+};
+
+console.log(minTimeToVisitAllPoints([[1, 1], [3, 4], [-1, 0]]))
