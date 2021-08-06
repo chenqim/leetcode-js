82319444ba406641e25fe4b88958242e4d6823c7
chenqiming <chenqiming@jiagouyun.com>
2021-08-06 11:14:30 +0800

    dodify 1507

--- Q1500-Q1599/1507_Reformat_Date.js
+++ Q1500-Q1599/1507_Reformat_Date.js
@@ -2,7 +2,7 @@
  * @param {string} date
  * @return {string}
  */
- var reformatDate = function(date) {
+var reformatDate = function(date) {
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
   const arr = date.split(' ')
   const day = arr[0].substring(0, arr[0].length - 2)
