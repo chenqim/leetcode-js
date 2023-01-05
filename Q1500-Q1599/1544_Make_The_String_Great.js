022b96af1d07cc9158efd6136e0a6948dbc9266a
chenqiming <chenqiming@jiagouyun.com>
2023-01-05 16:18:47 +0800

    add 1544

---
+++ Q1500-Q1599/1544_Make_The_String_Great.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeGood = function(s) {
+  const len = s.length
+  const tmp = []
+  let i = 0
+  while (i < len) {
+    const ch = s.charAt(i)
+    const last = tmp[tmp.length - 1] // 缓存数组中最后一个字符串
+    if (last && Math.abs(ch.charCodeAt() - last.charCodeAt()) === 32) {
+      tmp.pop()
+    } else {
+      tmp.push(ch)
+    }
+    i++
+  }
+  return tmp.join('')
+};
